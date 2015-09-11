d9f926bcefce2d429095462ee6a3e2882693b90a
hakon0601 <hakongimse@gmail.com>
2015-09-11 14:18:13 +0200

    del files

--- obstacle.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'hakon0601'
-
-
-class Obstacle():
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
