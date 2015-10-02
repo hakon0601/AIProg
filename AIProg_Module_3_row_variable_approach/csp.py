e264b53e6e073086c0f801bc9063f5557034fb4b
kariskjold <karifskjold@gmail.com>
2015-10-02 15:03:28 +0200

    Rewrite code - cleaner

--- AIProg_Module_3_row_variable_approach/csp.py
+++ AIProg_Module_3_row_variable_approach/csp.py
@@ -27,14 +27,47 @@ class CSP(csp_base.BaseCSP):
         reduced = False
         for constraining_variable_index in constr.involved_variables:
             constraining_variable = variable_dict[constraining_variable_index]
-            if variable.index != constraining_variable_index and len(constraining_variable.domain) == 1:
-                intersecting_value = constraining_variable.domain[0][variable.nr]
+            if variable.index != constraining_variable_index:
                 valid_domain = []
+                if len(constraining_variable.domain) == 1:
+                    intersecting_value = constraining_variable.domain[0][variable.nr]
+                    # Constraining variable has a singleton domain
+                    for permutation in variable.domain:
+                        if permutation[constraining_variable.nr] == intersecting_value:
+                            valid_domain.append(permutation)
+                        else:
+                            reduced = True
+                    variable.domain = valid_domain
+                else:
+                    # Domain of constraining variable is not a singleton domain, but if either
+                    # 1) none of the permutations in constraining variable has a True value of the intersecting cell
+                    # -> remove from variable.domain those permutation that has a True value in intersecting cell
+                    # 2) all of the permutations in constraining variable has a True value of the intersecting cell
+                    # -> remove from variable.domain those permutation that has a False value in intersecting cell
 
-                for permutation in variable.domain:
-                    if permutation[constraining_variable.nr] == intersecting_value:
-                        valid_domain.append(permutation)
+                    permutation_count = len(constraining_variable.domain)
+
+                    intersecting_value_is_true = 0
+                    intersecting_value_is_false = 0
+
+                    for permutation in constraining_variable.domain:
+                        if permutation[variable.nr] == True:
+                            intersecting_value_is_true += 1
+                        elif permutation[variable.nr] == False:
+                            intersecting_value_is_false +=1
+
+                    if intersecting_value_is_true == permutation_count or intersecting_value_is_false == permutation_count:
+                        for permutation in variable.domain:
+                            if intersecting_value_is_true == permutation_count and permutation[constraining_variable.nr] == True:
+                                valid_domain.append(permutation)
+                            elif intersecting_value_is_false == permutation_count and permutation[constraining_variable.nr] == False:
+                                valid_domain.append(permutation)
+                            else:
+                                reduced = True
                     else:
-                        reduced = True
-                variable.domain = valid_domain
+                        # If constraining variable has permutations that has a mix of True and False for the intersecting cell,
+                        # Then we can not reduce variable domain
+                        for permutation in variable.domain:
+                            valid_domain.append(permutation)
+                    variable.domain = valid_domain
         return reduced
